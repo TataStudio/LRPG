7fadcda886b33adadbe236b2bcae0fca1ebf2581
TataStudio <32775295+TataStudio@users.noreply.github.com>
2023-08-25 15:50:48 +0900

    login test

--- src/App.js
+++ src/App.js
@@ -4,76 +4,47 @@ import RegisterForm from './RegisterForm';
 import HomePage from './HomePage';
 import LoginForm from './LoginForm';
 import NavigationButtons from './NavigationButtons';
-import './App.css'; // Make sure to import your CSS file here
+import './App.css';
+
+const BASE_URL = 'https://lrpg.servegame.com';
 
 function App() {
-  const [registeredUsers, setRegisteredUsers] = useState({});
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [showRegisterModal, setShowRegisterModal] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
 
-
-  const handleRegister = (username, email, password) => {
-    if (registeredUsers[username]) {
-      alert('A user with this username is already registered');
-      return;
-    }
-    setRegisteredUsers({
-      ...registeredUsers,
-      [username]: { email, password },
-    });
-    alert('Registration successful!');
-  };
-
-  const handleLogin = (username, password) => {
-    const user = registeredUsers[username];
-    if (!user || user.password !== password) {
-      alert('Invalid username or password');
-      return;
-    }
-    setLoggedInUser(username);
-    alert('Login successful!');
-  };
-
-  const handleLogout = () => {
-    setLoggedInUser(null);
-  };
-
-  const openRegisterModal = () => {
-    setShowRegisterModal(true);
-  };
-
-  const closeRegisterModal = () => {
-    setShowRegisterModal(false);
-  };
-
-  const openLoginModal = () => {
-    setShowLoginModal(true);
-  };
-  
-  const closeLoginModal = () => {
-    setShowLoginModal(false);
-  };
-  
+  const handleRegister = async (username, email, password) => { /* ... */ };
+  const handleLogin = async (username, password) => { /* ... */ };
+  const handleLogout = () => { setLoggedInUser(null); };
 
   return (
     <Router>
       <div>
         <h1>Welcome to RPG Life!</h1>
-        <NavigationButtons loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} handleLogout={handleLogout} onRegisterClick={openRegisterModal} onLoginClick={openLoginModal} />
+        <NavigationButtons 
+          loggedInUser={loggedInUser} 
+          setLoggedInUser={setLoggedInUser} 
+          handleLogout={handleLogout} 
+          onRegisterClick={() => setShowRegisterModal(true)} 
+          onLoginClick={() => setShowLoginModal(true)} 
+        />
         <Routes>
-          <Route path="/" element={loggedInUser !== 'login' && loggedInUser !== 'register' ? <HomePage loggedInUser={loggedInUser} onLogout={handleLogout} /> : null} />
+          <Route path="/" element={<HomePage loggedInUser={loggedInUser} onLogout={handleLogout} />} />
         </Routes>
-        {showRegisterModal && <div className="modal">
-          <div className="modal-content">
-            <RegisterForm onRegister={handleRegister} onClose={closeRegisterModal} />
+        {showRegisterModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <RegisterForm onRegister={handleRegister} onClose={() => setShowRegisterModal(false)} />
+            </div>
           </div>
-        </div>}
-        {showLoginModal && <div className="modal">
-          <div className="modal-content">
-            <LoginForm onLogin={handleLogin} onClose={closeLoginModal} />
+        )}
+        {showLoginModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <LoginForm onLogin={handleLogin} onClose={() => setShowLoginModal(false)} />
+            </div>
           </div>
-        </div>}
+        )}
       </div>
     </Router>
   );
