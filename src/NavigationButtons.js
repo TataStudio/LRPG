1ce9994337d1358b5ee791337fd8ab258ceddaf9
TataStudio <32775295+TataStudio@users.noreply.github.com>
2023-08-30 18:22:17 +0900

    login Landing Test 13

--- src/NavigationButtons.js
+++ src/NavigationButtons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function NavigationButtons({ loggedInUser, handleLogout, onRegisterClick, onLoginClick }) {
+function NavigationButtons({ loggedInUser, onLogout: handleLogout, onRegisterClick, onLoginClick }) {
   const navigate = useNavigate();
 
   const goToProfile = () => {
