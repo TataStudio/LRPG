c702197a67a20a2e94bfd35bc0f3bc8b784d4571
TataStudio <32775295+TataStudio@users.noreply.github.com>
2023-08-30 18:10:22 +0900

    login Landing Test 12

--- src/HomePage.js
+++ src/HomePage.js
@@ -18,23 +18,12 @@ function HomePage({ loggedInUser, onLogout }) {
           <h3>Welcome back, {loggedInUser}!</h3>
           <p>Here's your personalized dashboard content.</p>
           {/* You can add more personalized user content here */}
-          
-          <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
         <div>
           <p>Please log in to access your personalized content.</p>
         </div>
       )}
-
-      {/* Add navigation options for different sections of your site */}
-      <nav>
-        <ul>
-          <li><button onClick={() => navigate('/profile')}>Profile</button></li>
-          <li><button onClick={() => navigate('/settings')}>Settings</button></li>
-          {/* Add more navigation options as needed */}
-        </ul>
-      </nav>
     </div>
   );
 }
