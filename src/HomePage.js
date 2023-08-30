8508afed6596c8bcd2603fc66eb11f98d2bcbfaf
TataStudio <32775295+TataStudio@users.noreply.github.com>
2023-08-30 18:36:31 +0900

    login Landing Test 14

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,21 +9,21 @@ function HomePage({ loggedInUser, onLogout }) {
     navigate('/login');
   };
 
+  if (loggedInUser) {
+    return (
+      <div className="homepage-container">
+        <h2>Welcome to LRPG</h2>
+        <h3>Welcome back, {loggedInUser}!</h3>
+        <p>Here's your personalized dashboard content.</p>
+        {/* You can add more personalized user content here */}
+      </div>
+    );
+  } 
+
   return (
     <div className="homepage-container">
       <h2>Welcome to LRPG</h2>
-
-      {loggedInUser ? (
-        <div>
-          <h3>Welcome back, {loggedInUser}!</h3>
-          <p>Here's your personalized dashboard content.</p>
-          {/* You can add more personalized user content here */}
-        </div>
-      ) : (
-        <div>
-          <p>Please log in to access your personalized content.</p>
-        </div>
-      )}
+      <p>Please log in to access your personalized content.</p>
     </div>
   );
 }
