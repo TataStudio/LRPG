0f99941c5d191b4c6ac4e0d56d0de0918f0061a3
TataStudio <32775295+TataStudio@users.noreply.github.com>
2023-12-07 16:46:35 +0900

    231207_login_test3

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,12 +1,24 @@
-import React from 'react';
+// LoginPage.js
+import React, { useState } from 'react';
 import LoginForm from './LoginForm';
 
 function LoginPage() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogin = (username) => {
+    setLoggedInUser(username);
+  };
+
   return (
     <div className="login-page">
       <h2>Login</h2>
-      <LoginForm />
-      {/* You can add more content or styling here if needed */}
+      <LoginForm onLogin={handleLogin} />
+      {loggedInUser && (
+        <div className="welcome-message">
+          <p>Welcome, {loggedInUser}!</p>
+          {/* You can add more personalized content here */}
+        </div>
+      )}
     </div>
   );
 }
